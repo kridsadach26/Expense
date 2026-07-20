92f0a7b5e0e3839140342cb9031f5babd0acfc85
kridsadach26 <kridsada.ch26@gmail.com>
2026-07-21 00:55:04 +0700

    Delete supabase directory

--- supabase/admin-setup.sql
+++
@@ -1,7 +0,0 @@
--- 1) สร้าง Admin User ใน Supabase Authentication > Users ก่อน
--- 2) คัดลอก User UUID
--- 3) แทนค่า ADMIN_AUTH_USER_UUID แล้วรันไฟล์นี้
-
-insert into public.admin_users (user_id)
-values ('ADMIN_AUTH_USER_UUID')
-on conflict (user_id) do nothing;
